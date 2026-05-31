c1c901c7fce8a1e57d9e6d964b21629991fe007e
Jonathan <jinfanzon1989@gmail.com>
2026-05-31 00:35:32 -0500

    Cambio de apellido

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 locals {
-  idapp = "apellido" # Apellido
+  idapp = "infanzon" # Apellido
 }
 
 terraform {
@@ -13,5 +13,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b6dd1983-a328-4ef9-aae9-b401b616479d" # Id de suscripción
+  subscription_id = "c10df55d-4a84-452b-9e5a-4a58bd29decd" # Id de suscripción
 }
